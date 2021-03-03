8adf65086854287c06f9cd5bdbdaa381a3c30d4d
ikhodos <thank43!>
2021-03-03 16:20:07 -0500

    Fix spelling

--- ThreadManagement/ThreadManager.cs
+++ ThreadManagement/ThreadManager.cs
@@ -15,7 +15,7 @@ namespace ThreadManagement
         public void RunAsync(List<WorkUnit> task, int maxConcurrentThreads)
         {
             // TODO: Please implement a task manager that runs only the given (maxConcurrentThreads) number of threads at one time
-            // Do not use Parrallel.ForEach, this test is meant to test your thread management skills specifically
+            // Do not use Parallel.ForEach, this test is meant to test your thread management skills specifically
             // Implement this using any thread management technique of your choice
             // There should be no more than maxConcurrentThreads running at the same time
             // Ideally as close to the max number as possible should be running
